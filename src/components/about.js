1efd2aab60fccac386a5b598a10782614f117bcc
Ahmed Essam Halbas <40437835+AhmedHalbas@users.noreply.github.com>
2021-04-10 21:26:22 +0200

    Add About Component

---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+import { Alert } from 'react-bootstrap-v5';
+const About = () => {
+  return (
+    <div className=' d-flex justify-content-center align-items-center min-vh-100'>
+      <Alert className='fs-2' variant='success'>
+        About Dummy Page
+      </Alert>
+    </div>
+  );
+};
+
+export default About;
